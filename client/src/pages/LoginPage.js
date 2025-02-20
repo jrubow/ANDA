be5a3dbd6a727759076f4add55a594d278726dd8
James Lamprski <jlampar@purdue.edu>
2025-02-20 16:46:50 -0500

    Updated the login page UI

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "../css/pages/loginpage.css"; // Import CSS
 
 function LoginPage() {
-  const [isTrusted, setIsTrusted] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
+
   return (
     <div className="container">
       {/* Container */}
@@ -17,52 +17,34 @@ function LoginPage() {
         <h2 className="title">User Login</h2>
 
         {/* Input Fields */}
-        <input
-          type="text"
-          placeholder="Username"
-          className="input-field"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="input-field"
-        />
-
-        {/* Trust Checkbox */}
-        <label className="checkbox-label">
-          <input
-            type="checkbox"
-            checked={isTrusted}
-            onChange={() => setIsTrusted(!isTrusted)}
-            className="checkbox"
-          />
-          Trust this device
-        </label>
+        <input type="text" placeholder="Username" className="input-field" />
+        <input type="password" placeholder="Password" className="input-field" />
 
         {/* Admin Toggle */}
-        <label className="checkbox-label">
-          <input
-            type="checkbox"
-            checked={isAdmin}
-            onChange={() => setIsAdmin(!isAdmin)}
-            className="checkbox"
-          />
+        <label className="switch-label">
           Admin
+          <div className="switch">
+            <input
+              type="checkbox"
+              checked={isAdmin}
+              onChange={() => setIsAdmin(!isAdmin)}
+            />
+            <span className="slider"></span>
+          </div>
         </label>
 
         {/* Login Button */}
-        <button className="login-button">
-          Login
-        </button>
+        <button className="login-button">Login</button>
+
+        {/* Guest Button */}
+        <button className="login-button">Guest</button>
 
         {/* Links */}
         <div className="links">
-          <a href="#">Create Account</a> | 
-          <a href="#">Forgot Password?</a>
+          <a href="#">Create Account</a> | <a href="#">Forgot Password?</a>
         </div>
         <div className="terms">
-          <a href="#">Terms</a> | 
-          <a href="#">Privacy Policy</a>
+          <a href="#">Terms</a> | <a href="#">Privacy Policy</a>
         </div>
       </div>
     </div>
