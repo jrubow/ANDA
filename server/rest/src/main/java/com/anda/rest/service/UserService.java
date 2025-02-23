ac180615ddf3a1ebc040dec6316af5e3a44d5266
juhg9543 <149544788+juhg9543@users.noreply.github.com>
2025-02-23 17:16:14 -0500

    Add files via upload

--- server/rest/src/main/java/com/anda/rest/service/UserService.java
+++ server/rest/src/main/java/com/anda/rest/service/UserService.java
@@ -1,12 +1,13 @@
 package com.anda.rest.service;
 
 import com.anda.rest.model.User;
+import com.anda.rest.model.Coordinates;
 
 import java.util.List;
 
 /**
  * Interface for User services
- * @author Gleb Bereziuk (gl3bert)
+ * @author Gleb Bereziuk (gl3bert), @author Jinhoo Yoon (juhg9543)
  */
 
 public interface UserService {
@@ -14,5 +15,6 @@ public interface UserService {
     public String updateUser(User user);
     public String deleteUser(String username);
     public User getUser(String username);
+    public Coordinates getCoordinates(String username);
     public List<User> getAllUsers();
 }
