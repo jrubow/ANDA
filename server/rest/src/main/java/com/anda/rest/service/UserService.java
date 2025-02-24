d572a1a5e4a027a81014cbc973ff9bf82f325dff
juhg9543 <149544788+juhg9543@users.noreply.github.com>
2025-02-23 21:48:27 -0500

    Add files via upload
    Added function for adding a weather event

--- server/rest/src/main/java/com/anda/rest/service/UserService.java
+++ server/rest/src/main/java/com/anda/rest/service/UserService.java
@@ -1,18 +1,22 @@
 package com.anda.rest.service;
 
 import com.anda.rest.model.User;
+import com.anda.rest.model.Coordinates;
+import com.anda.rest.model.WeatherEvent;
 
 import java.util.List;
 
 /**
  * Interface for User services
- * @author Gleb Bereziuk (gl3bert)
+ * @author Gleb Bereziuk (gl3bert), @author Jinhoo Yoon (juhg9543)
  */
 
 public interface UserService {
     public String createUser(User user);
-    public String updateUser(User user);
-    public String deleteUser(String username);
-    public User getUser(String username);
+    public String updateUser(String key, String username, User user);
+    public String deleteUser(String key, String username);
+    public User getUser(String key, String username);
+    public Coordinates getCoordinates(String key, String username);
     public List<User> getAllUsers();
+    public WeatherEvent getWeatherEvent(int weatherId);
 }
