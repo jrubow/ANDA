f055c21823d092a9cb13311311bd6afb2841f307
James Lamprski <jlampar@purdue.edu>
2025-02-26 15:53:54 -0500

    Updated the home page and added css code. Also added link to homepage in navbar. 1 hour

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,7 +8,7 @@ function NavBar() {
     return (
         <ul className="navbar">
             <h1>ANDA</h1>
-            <li><MdHome className="react-icon"/></li>
+            <li><Link to="/home"><MdHome className="react-icon"/></Link></li>
             <div className="navbar-right">
                 {!loggedIn ? <li className="nav-item"><Link to="/login">Login</Link></li> : ''}
                 { !loggedIn ? <li className="nav-item"><Link to="/register">Register</Link></li> : ''}
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "../css/pages/homepage.css"; // Import CSS
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="container">
+      {/* Sidebar */}
+      <div className="sidebar">
+        <div className="alert-box">
+          <h2>West Lafayette</h2>
+          <p>2 Alerts</p>
+        </div>
+        <nav>
+          <ul>
+            <li><Link to="/report">Make a Report</Link></li>
+            <li><Link to="/navigate">Navigate</Link></li>
+            <li><Link to="/layers">Map Layers</Link></li>
+          </ul>
+        </nav>
+        <div className="bottom-menu">
+          <ul>
+            <li><Link to="/account">Account</Link></li>
+            <li><Link to="/logout">Log Out</Link></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Map Section */}
+      <div className="map-container">
+        <img src="/MainMap.png" alt="Map" className="map-image" />
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
