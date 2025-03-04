b4f3e6dff4dc44a647e5acea77481fa1da234a31
juhg9543 <149544788+juhg9543@users.noreply.github.com>
2025-03-04 17:04:33 -0500

    Delete server/rest/src/main/java/com/anda/rest/repository/ReportRepository.java

--- server/rest/src/main/java/com/anda/rest/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anda.rest.repository;
-
-import com.anda.rest.model.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ReportRepository extends JpaRepository <Report, Integer> {
-    Report findById(int id);
-}
